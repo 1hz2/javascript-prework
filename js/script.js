744c31675a141a33dbfc90bb3b1ef640769dbc4a
Dominik Domański <dominikd33@o2.pl>
2023-04-16 11:10:21 +0200

    add function display result

--- js/script.js
+++ js/script.js
@@ -22,20 +22,9 @@ function playGame(playerInput){
 	console.log('Wylosowana liczba to: ' + randomNumber);
 
 	let computerMove = getMoveName(randomNumber);
-	/*
-	if(randomNumber == 1){
-	computerMove = 'kamień';
-	} else if (randomNumber == 2){
-		computerMove = 'papier';
-	} else if (randomNumber == 3){
-		computerMove = 'nożyce';
-	}
-	*/
+
 	printMessage('Mój ruch to: ' + computerMove);
 
-	/*
-	let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-	*/
 	console.log('Gracz wpisał: ' + playerInput);
 
 	function getMoveName(playerInput){
@@ -53,41 +42,9 @@ function playGame(playerInput){
 
 	let playerMove = getMoveName(playerInput);
 
-	/*
-	if(playerInput == '1'){
-	playerMove = 'kamień';
-	} else if (playerInput == '2'){
-		playerMove = 'papier';
-	} else if (playerInput == '3'){
-		playerMove = 'nożyce';
-	}
-	*/
-
 	printMessage('Twój ruch to: ' + playerMove);
 
 
-	if( computerMove == 'kamień' && playerMove == 'papier'){
-		printMessage('Wygrywasz!');
-	} else if (computerMove == 'kamień' && playerMove == 'kamień'){
-		printMessage('Remis!');
-	} else if (computerMove == 'kamień' && playerMove == 'nożyce'){
-		printMessage('Przegrywasz!');
-	} else if( computerMove == 'papier' && playerMove == 'papier'){
-		printMessage('Remis!');
-	} else if (computerMove == 'papier' && playerMove == 'kamień'){
-		printMessage('Przegrywasz!');
-	} else if (computerMove == 'papier' && playerMove == 'nożyce'){
-		printMessage('Wygrywasz!');
-	} else if( computerMove == 'nożyce' && playerMove == 'papier'){
-		printMessage('Przegrywasz!');
-	} else if (computerMove == 'nożyce' && playerMove == 'kamień'){
-		printMessage('Wygrywasz!');
-	} else if (computerMove == 'nożyce' && playerMove == 'nożyce'){
-		printMessage('Remis!');
-	}
-
-
-	/* 
 	function displayResult(computerMove, playerMove){
 		if( computerMove == 'kamień' && playerMove == 'papier'){
 			printMessage('Wygrywasz!');
@@ -109,7 +66,7 @@ function playGame(playerInput){
 			printMessage('Remis!');
 		}
 	}
-	*/
+ displayResult(computerMove, playerMove);
 }
 
 function buttonClicked1(){
